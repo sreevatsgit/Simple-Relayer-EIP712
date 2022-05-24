50bb58bc7bd74f1d7f7a5149d3c71c66da6118e1
Sreevatsan <sreevats.git@gmail.com>
2022-05-24 09:19:13 +0800

    contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers, upgrades} = require('hardhat');
 async function main(){
     const Storage = await ethers.getContractFactory('Storage');
     console.log('Storage contract being Deployed!');
-    const store = await upgrades.deployProxy(Storage, ["Default"],   { initializer: 'setQuote' });
+    const store = await upgrades.deployProxy(Storage, ["Default"],   { initializer: 'setMessage' });
     await store.deployed();
     console.log("Contract address:", store.address);
 }
