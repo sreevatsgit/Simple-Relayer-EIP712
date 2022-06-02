b8db787556b2ddbc1c52ba6814b45023d4ba6349
Sreevatsan <85744801+sreevatsgit@users.noreply.github.com>
2022-06-02 08:04:34 +0800

    Update EIPBaseContract.sol

--- contracts/BaseContracts/EIPBaseContract.sol
+++ contracts/BaseContracts/EIPBaseContract.sol
@@ -2,18 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract EIPBaseContract {
+
+// Defining a domain sep [click link here] (https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator)
+
     struct EIP712Domain {
         string name;
         string version;
         address verifyingContract;
         bytes32 salt;
     }
-
+    
+// Typehash [click link here] (https://eips.ethereum.org/EIPS/eip-712#rationale-for-typehash)
     bytes32 internal constant EIP712_DOMAIN_TYPEHASH = keccak256(bytes("EIP712Domain(string name,string version,address verifyingContract,bytes32 salt)"));
 
     bytes32 internal domainSeparator;
     bool private initialized;
-    
+
+//defining a domain seperator     
      function initialize( string memory name, string memory version) public returns(bytes32) {
         
          domainSeparator = keccak256(abi.encode(
